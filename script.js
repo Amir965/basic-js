6f6d595b88bc31d1fa4b207e630376e2d2c3862e
Amir965 <sohailaamir965@gmail.com>
2020-02-29 22:06:02 +0530

    js

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// var a=22;
+// var b="21";
+// var c=true;
+// var d=null;
+// var e;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// //conditional statement
+// if(a<b){
+// console.log("b is greater");
+// }
+// else if(a<c){
+//     console.log("c is greater");
+// }
+// else{
+//     console.log("a is greater");
+// }
+// var a=0;
+// switch(a){
+//     case 0:
+//         console.log("a is zero");
+//         break;
+//     case 1:
+//      console.log("a is +ve");
+//      break;
+//      default:
+//          console.log("a is -ve");
+//          break;
+
+// }
+//looping statement
+// var n=5;
+// for(i=0;i<n;i++)
+// {
+//     console.log(i);
+
+// }
+// var i=0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+//   }
+// var i=0;
+// do {
+//     console.log(i);
+//     i++;
+//   }
+//   while (i <=10);
+//Function
+function add(a,b){
+    var c=a+b;
+    return c;
+
+}
+console.log(add(4,3));
